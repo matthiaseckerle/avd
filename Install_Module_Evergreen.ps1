46367ab392c5dc972f56c3abb63c67dbcba635a4
meckerle <matthias.eckerle@braincon.de>
2023-05-30 13:27:39 +0200

    7Zip, NPP, NAPS2

--- Install_Module_Evergreen.ps1
+++ Install_Module_Evergreen.ps1
@@ -1,2 +1,21 @@
-Install-Module -Name Evergreen
+if (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+}
+
+$Installed = Get-Module -Name "Evergreen" -ListAvailable | `
+    Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
+    Select-Object -First 1
+$Published = Find-Module -Name "Evergreen"
+if ($Null -eq $Installed) {
+    Install-Module -Name "Evergreen"
+}
+elseif ([System.Version]$Published.Version -gt [System.Version]$Installed.Version) {
+    Update-Module -Name "Evergreen"
+}
+
 Import-Module -Name Evergreen
+
+Get-EvergreenApp 7zip
+Get-EvergreenApp Naps2
+Get-EvergreenApp NotepadPlusPlus
