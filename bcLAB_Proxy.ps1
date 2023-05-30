edfd9534270bd6e24559686244b719acca5ea6ca
meckerle <matthias.eckerle@braincon.de>
2023-05-30 14:13:40 +0200

    wpad

--- bcLAB_Proxy.ps1
+++ bcLAB_Proxy.ps1
@@ -1,2 +1,1 @@
-Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n255.255.255.255`twpad.lab.braincon.de" -Force
-#
+Add-Content -Path C:\Temp\hosts -Value "`n255.255.255.255`twpad`twpad.lab.braincon.de" -Force
